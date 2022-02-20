26dc93711d0afcbc05953eae734536c946736cf3
yangyuqian <1119374808@qq.com>
2022-02-20 22:09:40 +0800

    a

--- src/views/alive/hot-product/index.vue
+++ src/views/alive/hot-product/index.vue
@@ -1,103 +1,137 @@
 <template class="base-main">
   <div>
-      <yyq-table
-        :tableColumn="rankColumn"
-        :tableData="tableData"
-        :paginationConfig="{ total, pageSize }"
-        @handleCurrentChange="handleCurrentChange"
-      >
-        <template #rank="{ row }">
-          <div>
-            {{row.ranking}}
-          </div>
-          <div>
-            {{row.rankingIncrement}}
-          </div>
-        </template>
-        <template #product="{ row }">
-            <div class="content-slot">
-                <el-popover placement="top" width="240" trigger="hover">
-                    <template #reference>
-                        <el-image class="live-cover" :src="row.image"></el-image>
-                    </template>
-                    <template>
-                        <el-image style="width: 100%; hegiht: 100%;" :src="row.image"></el-image>
-                    </template>
-                </el-popover>
-                <div>
-                    {{row.title}}
-                </div>
-                <div>
-                    售价{{row.price}}
-                </div>
-                <div>
-                    {{row.marketPrice}}
-                </div>
+    <yyq-table
+      :tableColumn="rankColumn"
+      :tableData="tableData"
+      :paginationConfig="{ total, pageSize }"
+      @handleCurrentChange="handleCurrentChange"
+    >
+      <template #rank="{ row }">
+        <span>
+          {{row.ranking}}
+        </span>
+        <span class="tag-group">
+          <el-tag effect="plain">
+            <span>{{ row.rankingIncrement | tablefilter('rankingIncrement') || '-'}}</span>
+          </el-tag>
+        </span>
+      </template>
+      <template #product="{row}">
+        <div class="content-slot">
+          <el-popover
+            placement="top"
+            width="240"
+            trigger="hover"
+          >
+            <template #reference>
+              <el-image class="live-cover" :src="row.image"></el-image>
+            </template>
+            <template>
+              <el-image style="width: 100%; hegiht: 100%;" :src="row.image"></el-image>
+            </template>
+          </el-popover>
+          <div style="margin-left: 8px; ">
+            <div class="vertical_2" style="font-size: 14px;">{{row.title}}</div>
+            <div style="margin-top: 4px;">
+              <span>售价：</span>
+              <span class="price-css">￥{{ row.price | tablefilter('centToyuan') }}</span>
+              <span class="delete-line">{{ row.marketPrice |  tablefilter('centToyuan')  }}</span>
             </div>
-        </template>
-        <template #op="{ row }">
-            <el-button size="mini" type="primary" @click="handleInfo(row)">
-              商品详情
-            </el-button>
-        </template>
-      </yyq-table>
-    </div>
+          </div>
+        </div>
+      </template>
+      <template #op="{ row }">
+        <el-button size="mini" type="primary" @click="handleInfo(row)">
+          商品详情
+        </el-button>
+      </template>
+    </yyq-table>
+  </div>
 </template>
 
 <script>
-import yyqTable from '../modules/yyq-table.vue';
-import { rankColumn } from "./config";
-import LiveApi from "@/api/liveApi";
-export default {
-  components: {
-    yyqTable,
-  },
-  data() {
-    return {
-      dialogVisible: false,
-      recordData: {
-        user: {},
-        planData: {},
-      },
-      rankColumn,
-      tableData: [],
-      total: 0,
-      page: 1,
-      pageSize: 10,
-    };
-  },
-  mounted() {
+  import yyqTable from '../modules/yyq-table.vue';
+  import {rankColumn} from "./config";
+  import LiveApi from "@/api/liveApi";
+
+  export default {
+    components: {
+      yyqTable,
+    },
+    data() {
+      return {
+        dialogVisible: false,
+        recordData: {
+          user: {},
+          planData: {},
+        },
+        rankColumn,
+        tableData: [],
+        total: 0,
+        page: 1,
+        pageSize: 10,
+      };
+    },
+    mounted() {
       this.getLiveProductSaleRank();
-  },
-  methods: {
+    },
+    methods: {
       getLiveProductSaleRank() {
-          const params = {
-              page: this.page,
-              pageSize: this.pageSize
-          }
-          LiveApi.getLiveProductSaleRank(params).then(res => {
-              this.total = res.size;
-              res.data.forEach((item, index) => {
-                  item.ranking = (index + 1) + ((this.page - 1) * 10);
-              })
-              this.tableData = res.data;
+        const params = {
+          page: this.page,
+          pageSize: this.pageSize
+        }
+        LiveApi.getLiveProductSaleRank(params).then(res => {
+          this.total = res.size;
+          res.data.forEach((item, index) => {
+            item.ranking = (index + 1) + ((this.page - 1) * 10);
           })
+          this.tableData = res.data;
+        })
       },
-    handleCurrentChange(val) {
+      handleCurrentChange(val) {
         this.page = val;
         this.getLiveHotRoomHistory();
+      },
+      handleInfo(row) {
+
+      }
     },
-    handleInfo(row) {
-        
-    }
-  },
-};
+  };
 </script>
 
 <style lang="scss" scoped>
-    .live-cover{
-        width: 50px;
-        height: 50px;
-        border-radius: 10px;
+  .live-cover {
+    width: 50px;
+    height: 50px;
+    border-radius: 10px;
+  }
+
+  .content-slot {
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    line-height: 16px;
+  }
+
+  .delete-line {
+    text-decoration: line-through;
+  }
+
+  .price-css {
+    font-weight: bold;
+    color: orangered;
+  }
+
+  .tag-group {
+    & /deep/ .el-tag {
+      width: 50px;
+      padding: 0px;
+      margin-left: 10px;
+      height: 20px;
+      border-radius: 30px;
+      font-weight: bold;
+      line-height: 20px;
     }
+  }
 </style>
--- src/views/alive/live-real/config.js
+++ src/views/alive/live-real/config.js
@@ -1,18 +1,18 @@
 export const salesColumn = [
-    {prop: 'ranking',  label: '排名', width: '70'},
+    {prop: 'ranking', align: 'center',  label: '排名', width: '70'},
     {prop: 'live',  label: '直播', slotName: 'live', width: '400'},
-    {prop: 'intelligent',  label: '达人', slotName: 'intelligent', width: '400'},
-    {prop: 'currentUserCount',  label: '当前在线人数', NumConfig: {filterName: 'toWan'} },
+    {prop: 'intelligent',  label: '达人', slotName: 'intelligent', width: '300'},
+    {prop: 'currentUserCount',  label: '当前在线人数', NumConfig: {filterName: 'toWan'}, width: '120' },
     {prop: 'likeCount',  label: '获得点赞数', NumConfig: {filterName: 'toWan'} },
     {prop: 'platformSalesCount',  label: '累计销量', NumConfig: {filterName: 'toWan'} },
     {prop: 'lastSalesPrice',  label: '预估销售额', NumConfig: {filterName: 'toWan', unit: '￥', style: {color: 'gold'}} },
     {prop: 'op',  label: '操作', slotName: 'op'}
 ]
 export const realColumn = [
-    {prop: 'ranking',  label: '排名', width: '70'},
+    {prop: 'ranking', align: 'center',  label: '排名', width: '70'},
     {prop: 'live',  label: '直播', slotName: 'live', width: '400'},
-    {prop: 'intelligent',  label: '达人', slotName: 'intelligent', width: '400'},
-    {prop: 'currentUserCount',  label: '当前在线人数', NumConfig: {filterName: 'toWan'} },
+    {prop: 'intelligent',  label: '达人', slotName: 'intelligent', width: '300'},
+    {prop: 'currentUserCount',  label: '当前在线人数', NumConfig: {filterName: 'toWan'}, width: '120' },
     {prop: 'likeCount',  label: '获得点赞数', NumConfig: {filterName: 'toWan'} },
     {prop: 'platformSalesCount',  label: '累计销量', NumConfig: {filterName: 'replace', replace: '未带货'} },
     {prop: 'lastSalesPrice',  label: '预估销售额', NumConfig: {filterName: 'replace', replace: '未带货'} },
